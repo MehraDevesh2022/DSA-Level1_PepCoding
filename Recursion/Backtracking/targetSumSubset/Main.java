04372ff4521e0c3b22475d9d1c2f5a71663090ad
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-09-06 12:38:00 +0530

    target Sum

---
+++ Recursion/Backtracking/targetSumSubset/Main.java
@@ -0,0 +1,78 @@
+/*Target Sum Subsets
+Easy  Prev   Next
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are given a number "tar".
+4. Complete the body of printTargetSumSubsets function - without changing signature - to calculate and print all subsets of given elements, the contents of which sum to "tar". Use sample input and output to get more idea.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+A number tar
+Output Format
+Comma separated elements of the subset, the contents of which add to "tar"
+.. all such subsets, each in a single line (the elements of each subset should be comma separated)
+Question Video
+
+  COMMENTConstraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 20
+0 <= tar <= 50
+Sample Input
+5
+10
+20
+30
+40
+50
+60
+Sample Output
+10, 20, 30, .
+10, 50, .
+20, 40, .
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void input(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        input(arr);
+        int tar = scn.nextInt();
+        printTargetSumSubsets(arr, 0, "", 0, tar);
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+        if (sos > tar) {
+            return;
+        }
+        if (idx == arr.length) {
+            if (sos == tar)
+                System.out.println(set + ".");
+            return;
+        }
+
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ", ", sos + arr[idx], tar);
+        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+
+    }
+
+}
