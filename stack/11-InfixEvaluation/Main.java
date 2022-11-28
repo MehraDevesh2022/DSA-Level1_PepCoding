d8ad30bc1e82ca0ee125ee41f7b24a3d6ad7e39d
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-11-28 23:33:19 +0530

    Infix Evaluation added

---
+++ stack/11-InfixEvaluation/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Infix Evaluation
+ * Easy Prev Next
+ * 1. You are given an infix expression.
+ * 2. You are required to evaluate and print it's value.
+ * Input Format
+ * Input is managed for you
+ * Output Format
+ * Value of infix expression
+ * Question Video         
+ * 
+ * COMMENTConstraints
+ * 1. Expression is balanced
+ * 2. The only operators used are +, -, *, /
+ * 3. Opening and closing brackets - () - are used to impact precedence of
+ * operations
+ * 4. + and - have equal precedence which is less than * and /. * and / also
+ * have equal precedence.
+ * 5. In two operators of equal precedence give preference to the one on left.
+ * 6. All operands are single digit numbers.
+ * Sample Input
+ * 2 + 6 * 4 / 8 - 3
+ * Sample Output
+ * 2
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();                          
+        // code
+    }
+} 
