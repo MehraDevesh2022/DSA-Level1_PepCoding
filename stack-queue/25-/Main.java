d7b2aa4656c75a59c5042fbba08605e6d17c120b
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-08 23:51:24 +0530

    Stack To Queue Adapter - Remove Efficient

---
+++ stack-queue/25-/Main.java
@@ -0,0 +1,65 @@
+/*
+ * Stack To Queue Adapter - Remove Efficient
+ * Easy Prev Next
+ * 1. You are required to complete the code of our StackToQueueAdapter class.
+ * The class should mimic the behaviour of a Queue and implement FIFO semantic.
+ * 2. Here is the list of functions that are written in the class
+ * 2.1. add -> Accepts new data if there is space available in the underlying
+ * array or
+ * print "Queue overflow" otherwise.
+ * 2.2. remove -> Removes and returns value according to FIFO, if available or
+ * print
+ * "Queue underflow" otherwise and return -1.
+ * 2.3. peek -> Returns value according to FIFO, if available or print "Queue
+ * underflow" otherwise and return -1.
+ * 2.4. size -> Returns the number of elements available in the queue.
+ * 3. Input and Output is managed for you.
+ * Input Format
+ * Input is managed for you
+ * Output Format
+ * Output is managed for you
+ * Question Video
+ * 
+ * COMMENTConstraints
+ * Note -> remove, peek and size should work in constant time. add should work
+ * in linear time.
+ * Sample Input
+ * add 10
+ * add 20
+ * add 30
+ * add 40
+ * add 50
+ * remove
+ * remove
+ * add 60
+ * add 70
+ * peek
+ * remove
+ * peek
+ * remove
+ * peek
+ * remove
+ * peek
+ * remove
+ * peek
+ * remove
+ * peek
+ * remove
+ * quit
+ * Sample Output
+ * 10
+ * 20
+ * 30
+ * 30
+ * 40
+ * 40
+ * 50
+ * 50
+ * 60
+ * 60
+ * 70
+ * 70
+ * Queue underflow
+ * Queue underflow
+ * 
+ */
