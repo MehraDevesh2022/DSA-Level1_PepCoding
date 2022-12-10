68317efbcc70f10a071ede8f249581c776b3aa64
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-10 23:58:17 +0530

    AutoBoxing def added

--- Warpper-class and AutoBoxing/Main.java
+++ Warpper-class and AutoBoxing/Main.java
@@ -5,6 +5,9 @@
  //      Integer x =3; and Integer x ; where   x is null bydefault
  
  
+// AutoBoxing def. ==> Autoboxing is the automatic conversion that the Java compiler makes between the primitive types and their corresponding object wrapper classes. For example, converting an int to an Integer, a double to a Double, and so on. If the conversion goes the other way, this is called unboxing. 
+
+
  // Autoboxing eg :
 
  public class Main{
