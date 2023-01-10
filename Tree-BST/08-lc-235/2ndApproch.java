37b58c924da0f07d73403b64c07d35dbd0f53b37
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-01-10 17:59:32 +0530

    generic tree added

---
+++ Tree-BST/08-lc-235/2ndApproch.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (p.val < curr.val && q.val < curr.val) {
+                curr = curr.left;
+            } else if (p.val > curr.val && q.val > curr.val) {
+                curr = curr.right;
+            } else {
+                return curr;
+            }
+        }
+        return curr;
+    }
+}
