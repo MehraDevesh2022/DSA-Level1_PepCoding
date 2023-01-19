202380a7d7ca6dc9bd4590da10cd15a1a60929a4
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-01-19 18:07:26 +0530

    Remove Leaves In Generic Tree

--- Tree-Generic/11-/Main.java
+++ Tree-Generic/11-/Main.java
@@ -164,14 +164,29 @@ public class Main {
 
   public static void removeLeaves(Node node) {
     // write your code here
-     for(int i = node.children.size()-1; i>=0 ; i-- ){
+    /* Approch =>
+      we are using pre order here . Q ki post order main pehle leafs ke liye kaam hota hai.. agr hum sare leafs ke liye pehle kaam kr le or unko delete kr le to contole jb return hooga none leaf node main to wo apna kaam krega or cheack krega kya wo leaf hai . qki hum sb leaf pehle hi delete kr chuke honge to cindtion true hogi or aise sare nodes delete ho jainge or value bchegi null. 
+       pre order main suru se leaf check kringe eventualy jb leaf milega to sare nodes ke kaam ho chuka hoga and we will remove leaf only.
+           > start loop from nodes children arrayList last value because. we cant remove first value from arrayList so start from last.
+           > get the child node from last node of children arrayList of root node.
+           > now cheak if child children  array list size  ==0 to wo leaf node because there is no nodes further after that node. 
+           > once condtion true remove that node chilren from i th position from array list. we are removing from last to 0 so there is no effcet on arrayList
+           > check for all child .. if may child.chldren value is greater then 0 that mean that child node is not leaf. 
+           > now call for nodes child .. now child become root node and check if that node childe.children size ==0 .
+            eveantually all leaf nodes are gone. and return 
+
+     * 
+     */
+     for(int i = node.children.size()-1; i>=0 ; i-- ){ // start from last node bcz we cant remove value from staring of array list so remove from last
+
         // get the child 
-        Node child  = node.children.get(i);
-        if(child.children.size() ==0){
+        Node child  = node.children.get(i); // get i th postion child . if there any
+        if(child.children.size() ==0){  // now check is that childs has children. if not that mean that child i self is leaf and remove that from children array . and check for another once
             node.children.remove(i);
         }
 
      }
+     // if may some nodes has children availble so call those child and they will check for there childern eventually all leafs will founded and removed
      for(Node child : node.children){
         removeLeaves(child);
      }
