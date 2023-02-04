7c7acf4e337d4524e5da7a96c64cd77852c7d26a
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-02-04 15:26:18 +0530

    Transform To Normal From Left-cloned Tree added

---
+++ Tree/16-TransformToNormalFromLeft-clonedTree/Main.java
@@ -0,0 +1,148 @@
+/*
+"I came in with near zero programming knowledge and halfway in, I'm quite confident of what I can achieve.
+~ Kelechi I."
+
+ * Transform To Normal From Left-cloned Tree
+Easy  Prev   Next
+1. You are given a partially written BinaryTree class.
+2. You are required to complete the body of transBackFromLeftClonedTree function. The function is expected to convert a left-cloned tree back to it's original form. The left cloned tree is dicussed in previous question. In a left-clone tree a new node for every node equal in value to it is inserted between itself and it's left child. For clarity check out the question video.
+3. Input and Output is managed for you.
+Input Format
+Input is managed for you.
+Output Format
+Output is managed for you. 
+Question Video
+
+  COMMENTConstraints
+None
+Sample Input
+37
+50 50 25 25 12 12 n n n n 37 37 30 30 n n n n n n 75 75 62 62 n n 70 70 n n n n 87 87 n n n
+Sample Output
+25 <- 50 -> 75
+12 <- 25 -> 37
+. <- 12 -> .
+30 <- 37 -> .
+. <- 30 -> .
+62 <- 75 -> 87
+. <- 62 -> 70
+. <- 70 -> .
+. <- 87 -> .
+
+ */
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static Node transBackFromLeftClonedTree(Node node) {
+        // write your code here
+
+        if (node == null) {
+            return null;
+        }
+
+        Node leftAns = transBackFromLeftClonedTree(node.left.left);
+        transBackFromLeftClonedTree(node.right);
+        node.left = leftAns;
+        return node;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
+            } else {
+                arr[i] = null;
+            }
+        }
+
+        Node root = construct(arr);
+        root = transBackFromLeftClonedTree(root);
+        display(root);
+    }
+
+}
