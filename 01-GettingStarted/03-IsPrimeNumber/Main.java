ef8efa27edeeaedca2326640d85ecfed7b9e2e70
Devesh Mehara <mehra.devesh2022@gmail.com>
2024-01-06 23:51:03 +0530

    Update Main.java

--- 01-GettingStarted/03-IsPrimeNumber/Main.java
+++ 01-GettingStarted/03-IsPrimeNumber/Main.java
@@ -61,10 +61,8 @@ public class Main {
 
   public static boolean isPrime(int number){
       for(int i=2; i*i<=number; i++){
-        if(number%i ==0){
-            return false;
-        }
-      }
+        if(number %i ==0)   return false;
+           }
       return true;
   }
          
